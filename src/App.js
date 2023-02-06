8bc07f20658f82a4a16b3988ddc01cfcdbc27d01
ndhieu2701 <hieu@v7rx3.onmicrosoft.com>
2023-02-06 09:29:59 +0700

    update code to deploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import DefaultLayout from "./Components/Layout/DefaultLayout";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Routes>
           {publicRoutes.map((route, index) => {
